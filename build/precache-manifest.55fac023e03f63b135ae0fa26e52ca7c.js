35577ee8dfe1aec21c99c4617ba81bcd5ddf6c94
Ishita Choudhary <69318862+chrysanthemum027@users.noreply.github.com>
2022-03-12 16:44:53 +0530

    build app

---
+++ build/precache-manifest.55fac023e03f63b135ae0fa26e52ca7c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9bbf79eed0200f3ee777db2e04baa6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a82f780f4fd06c1a2ac8",
+    "url": "/static/js/2.f14e1213.chunk.js"
+  },
+  {
+    "revision": "9ee37e374ae20bf968c8",
+    "url": "/static/js/main.3f12c8ba.chunk.js"
+  },
+  {
+    "revision": "6902260eb42cca1ae5cb",
+    "url": "/static/js/runtime-main.6aa3fc4a.js"
+  }
+]);
